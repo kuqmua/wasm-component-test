d8fbaf1c513f2b402a60613ee40665ac1ff25283
kuqmua <serezhashmakov@outlook.com>
2022-05-04 18:54:40 +0300

    pub fn sum(x: i32, y: i32) -> i32

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub fn sum(x: i32, y: i32) -> i32 {
+    x + y
 }
+
